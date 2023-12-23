46c348e721f9fe3f0ce4e7901d53cc779359747a
naywvi <nagib.lakhdari.pro@gmail.com>
2023-12-23 03:18:36 +0100

    user ban route

--- src/pe_api/requests/user_request.js
+++ src/pe_api/requests/user_request.js
@@ -9,16 +9,25 @@ async function generate_result(message, object_result) {
   return result;
 }
 
+async function user_exist(user_request, search = false) {
+  const user_exist = await UserModel.findOne({
+    user_first_name: user_request.request.user_first_name,
+    user_last_name: user_request.request.user_last_name,
+    user_birthday: user_request.request.user_birthday,
+  });
+  if (search) {
+    if (user_exist) throw error_message.already_exists;
+  } else if (!search) {
+    if (!user_exist) throw error_message.not_found;
+    else return user_exist;
+  }
+}
+
 module.exports = {
   create: async (user_request) => {
     try {
       //if create user request is valid, check if user already exists
-      const user_exist = await UserModel.findOne({
-        user_first_name: user_request.request.user_first_name,
-        user_last_name: user_request.request.user_last_name,
-        user_birthday: user_request.request.user_birthday,
-      });
-      if (user_exist) throw error_message.already_exists;
+      await user_exist(user_request, true);
 
       //cancel bypass admin
       if (user_request.request.user_rank_id === 99)
@@ -43,6 +52,7 @@ module.exports = {
   },
   read: async (user_request, query) => {
     try {
+      await user_exist(user_request);
     } catch (error) {
       throw error;
     }
@@ -61,6 +71,18 @@ module.exports = {
   },
   ban: async (user_request) => {
     try {
+      //if user already exists
+      const user = await user_exist(user_request);
+
+      //check if user is not already banned
+      const banned = await UserModel.updateOne(user, { user_banned: true });
+      if (!banned.modifiedCount) throw error_message.already_banned;
+
+      const result = await generate_result(
+        `${user.user_first_name} was banned`,
+        user
+      );
+      return result;
     } catch (error) {
       throw error;
     }
--- src/pe_api/routes/http/user/user_ban.js
+++ src/pe_api/routes/http/user/user_ban.js
@@ -1,4 +1,7 @@
 const format_query = require("../../../../utils/format_query");
+const error_message = require("../../../../utils/error");
+const is_valid = require("../../../auth3/auth_token");
+const check_user_request = require("../../../requests/user_request");
 
 module.exports = {
   name: "/manage/user/ban",
@@ -6,10 +9,29 @@ module.exports = {
   method: "POST",
   run: async (req, res) => {
     try {
+      //<== format & check the request
+      const request = format_query.run(req.body);
+      const is_valid_token = await is_valid.check_validity_token(
+        request.sender.token,
+        request.sender._id
+      );
+      if (!is_valid_token) throw error_message.invalid_token;
+
+      //<== check the rank of the user
+      const rank = await is_valid.check_rank(
+        request.sender.token,
+        request.sender._id
+      );
+      if (rank !== 99 && rank !== 1 && rank !== 2 && rank !== 6)
+        throw error_message.unauthorized;
+
+      //<== ban the user
+      const result = await check_user_request.ban(request);
       res.status(200);
-      res.json("result");
+      res.json(result);
     } catch (error) {
       res.status(400);
+      console.log(error);
       res.json(error);
     } finally {
       res.end();
