2c26bc8812e90585b27e9ce503128d27533524f1
naywvi <nagib.lakhdari.pro@gmail.com>
2023-12-23 02:49:56 +0100

    user create route

--- src/pe_api/auth3/auth_token.js
+++ src/pe_api/auth3/auth_token.js
@@ -2,11 +2,21 @@ const mongoose = require("mongoose");
 const error_message = require("../../utils/error");
 
 module.exports = {
+  //<== check if token is valid
   check_validity_token: async (token, _id) => {
     const is_valid = await mongoose
       .model("User")
       .findOne({ _id: _id, user_token: token });
     if (!is_valid) return false;
     return true;
+  },
+  //<== check the rank of the user
+  check_rank: async (token, _id) => {
+    const is_valid = await mongoose
+      .model("User")
+      .findOne({ _id: _id, user_token: token });
+    if (!is_valid) throw error_message.unauthorized;
+    if (is_valid.user_admin) return 99;
+    return is_valid.user_rank_id;
   },
 };
--- src/pe_api/routes/http/user/user_create.js
+++ src/pe_api/routes/http/user/user_create.js
@@ -1,9 +1,11 @@
 const format_query = require("../../../../utils/format_query");
+const error_message = require("../../../../utils/error");
 const is_valid = require("../../../auth3/auth_token");
 const check_user_request = require("../../../requests/user_request");
+
 module.exports = {
   name: "/manage/user/c",
-  description: "Create a new user",
+  description: "Create a new user - rank 99 | 1 | 2 | 6",
   method: "POST",
   run: async (req, res) => {
     try {
@@ -14,6 +16,14 @@ module.exports = {
         request.sender._id
       );
       if (!is_valid_token) throw error_message.invalid_token;
+
+      //<== check the rank of the user
+      const rank = await is_valid.check_rank(
+        request.sender.token,
+        request.sender._id
+      );
+      if (rank !== 99 && rank !== 1 && rank !== 2 && rank !== 6)
+        throw error_message.unauthorized;
 
       //<== create the user
       const result = await check_user_request.create(request);
