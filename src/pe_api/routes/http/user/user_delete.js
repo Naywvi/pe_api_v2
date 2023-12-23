c0a0337d6aa805b1c97f870cc4b7a95138ff58e7
naywvi <nagib.lakhdari.pro@gmail.com>
2023-12-24 00:58:39 +0100

    not implemented & setup route delete user

--- src/pe_api/routes/http/user/user_delete.js
+++ src/pe_api/routes/http/user/user_delete.js
@@ -1,11 +1,11 @@
-const format_query = require("../../../../utils/format_query");
-
+const message_error = require("../../../../utils/error");
 module.exports = {
   name: "/manage/user/delete",
   description: "Delete a user or groupe of users",
   method: "POST",
   run: async (req, res) => {
     try {
+      throw message_error.not_implemented;
       res.status(200);
       res.json("result");
     } catch (error) {
