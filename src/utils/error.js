16fde161ec580b5352aa05e0a34c931efc8835f7
naywvi <nagib.lakhdari.pro@gmail.com>
2024-01-14 16:53:24 +0100

    resolve error again

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,122 +1,123 @@
 module.exports = {
-  badly_formatted: async () => ({
-    code: 400,
-    title: "Badly formatted",
-    message: "Contact an admin",
-  }),
-  not_enough_arguments: async () => ({
-    code: 400,
-    title: "Not enough arguments",
-    message: "If you continue to have this problem contact an admin",
-  }),
-  unknown_token: async () => ({
-    code: 400,
-    title: "Unknown token",
-    message: "Contact an admin",
-  }),
-  unauthorized: async () => ({
-    code: 401,
-    title: "Unauthorized",
-    message: "Contact an admin",
-  }),
-  not_found: async () => ({
-    code: 404,
-    title: "Not found",
-    message: "Contact an admin",
-  }),
-  update_failed: async () => ({
-    code: 403,
-    title: "Update failed",
-    message: "Contact an admin",
-  }),
-  already_exists: async () => ({
-    code: 409,
-    title: "Already exists",
-    message: "Contact an admin",
-  }),
-  server_error: async () => ({
-    code: 500,
-    title: "Server error",
-    message: "Contact an admin",
-  }),
-  not_implemented: async () => ({
-    code: 501,
-    title: "Not implemented",
-    message: "Contact an admin",
-  }),
   access_denied: async () => ({
     code: 403,
-    title: "Access denied",
-    message: "Contact an admin",
-  }),
-  invalid_data: async () => ({
-    code: 400,
-    title: "Invalid data",
-    message: "Contact an admin",
-  }),
-  invalid_token: async () => ({
-    code: 400,
-    title: "Invalid token",
-    message: "Contact an admin",
-  }),
-  invalid_password: async () => ({
-    code: 400,
-    title: "Invalid password",
-    message: "Contact an admin",
-  }),
-  invalid_email: async () => ({
-    code: 400,
-    title: "Invalid email",
-    message: "Contact an admin",
-  }),
-  invalid_username: async () => ({
-    code: 400,
-    title: "Invalid username",
-    message: "Contact an admin",
+    title: "Accès refusé",
+    message: "Contactez un administrateur",
   }),
   already_banned: async () => ({
     code: 400,
-    title: "Already banned",
-    message: "Contact an admin",
+    title: "Déjà banni",
+    message: "Contactez un administrateur",
+  }),
+  already_exists: async () => ({
+    code: 409,
+    title: "Déjà existant",
+    message: "Contactez un administrateur",
   }),
   already_unbanned: async () => ({
     code: 400,
-    title: "Already unbanned",
-    message: "Contact an admin",
+    title: "Déjà débanni",
+    message: "Contactez un administrateur",
   }),
   already_updated: async () => ({
     code: 400,
-    title: "Already updated",
-    message: "Contact an admin",
-  }),
-  no_modifications: async () => ({
-    code: 400,
-    title: "No modifications",
-    message: "Contact an admin",
-  }),
-  missing_information: async () => ({
-    code: 400,
-    title: "Missing information",
-    message: "Contact an admin",
-  }),
-  society_not_saved: async () => ({
-    code: 400,
-    title: "Society not saved",
-    message: "Contact an admin",
-  }),
-  cant_fire_yourself: async () => ({
-    code: 400,
-    title: "Can't fire yourself",
-    message: "Contact an admin",
-  }),
-  cant_unfire_yourself: async () => ({
-    code: 400,
-    title: "Can't unfire yourself",
-    message: "Contact an admin",
+    title: "Déjà mis à jour",
+    message: "Contactez un administrateur",
   }),
   bad_request: async () => ({
     code: 400,
-    title: "Bad request",
-    message: "Contact an admin",
+    title: "Requête incorrecte",
+    message: "Contactez un administrateur",
+  }),
+  badly_formatted: async () => ({
+    code: 400,
+    title: "Mauvaise formatation",
+    message: "Contactez un administrateur",
+  }),
+  cant_fire_yourself: async () => ({
+    code: 400,
+    title: "Impossible de vous licencier vous-même",
+    message: "Contactez un administrateur",
+  }),
+  cant_unfire_yourself: async () => ({
+    code: 400,
+    title: "Impossible de vous réintégrer vous-même",
+    message: "Contactez un administrateur",
+  }),
+  invalid_data: async () => ({
+    code: 400,
+    title: "Données invalides",
+    message: "Contactez un administrateur",
+  }),
+  invalid_email: async () => ({
+    code: 400,
+    title: "Adresse e-mail invalide",
+    message: "Contactez un administrateur",
+  }),
+  invalid_password: async () => ({
+    code: 400,
+    title: "Mot de passe invalide",
+    message: "Contactez un administrateur",
+  }),
+  invalid_token: async () => ({
+    code: 400,
+    title: "Token invalide",
+    message: "Contactez un administrateur",
+  }),
+  invalid_username: async () => ({
+    code: 400,
+    title: "Nom d'utilisateur invalide",
+    message: "Contactez un administrateur",
+  }),
+  missing_information: async () => ({
+    code: 400,
+    title: "Information manquante",
+    message: "Contactez un administrateur",
+  }),
+  not_enough_arguments: async () => ({
+    code: 400,
+    title: "Nombre d'arguments insuffisant",
+    message:
+      "Si vous continuez à avoir ce problème, contactez un administrateur",
+  }),
+  not_found: async () => ({
+    code: 404,
+    title: "Non trouvé",
+    message: "Contactez un administrateur",
+  }),
+  not_implemented: async () => ({
+    code: 501,
+    title: "Non implémenté",
+    message: "Contactez un administrateur",
+  }),
+  no_modifications: async () => ({
+    code: 400,
+    title: "Aucune modification",
+    message: "Contactez un administrateur",
+  }),
+  server_error: async () => ({
+    code: 500,
+    title: "Erreur du serveur",
+    message: "Contactez un administrateur",
+  }),
+  society_not_saved: async () => ({
+    code: 400,
+    title: "Société non enregistrée",
+    message: "Contactez un administrateur",
+  }),
+  unauthorized: async () => ({
+    code: 401,
+    title: "Non autorisé",
+    message: "Contactez un administrateur",
+  }),
+  unknown_token: async () => ({
+    code: 400,
+    title: "Token inconnu",
+    message: "Contactez un administrateur",
+  }),
+  update_failed: async () => ({
+    code: 403,
+    title: "Échec de la mise à jour",
+    message: "Contactez un administrateur",
   }),
 };
